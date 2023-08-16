1a20946040ae3bb5efd14d1256284e81546ae0ae
Nick DL <106737278+Nick-DL@users.noreply.github.com>
2023-08-16 11:14:13 +0800

    大更新

--- media/RedirectToGitHub.js
+++ media/RedirectToGitHub.js
@@ -13,3 +13,25 @@ function redirectToGitHub() {
   
     window.location.href = gitHubURL;
   }
+
+function EditToGitHub() {
+    var currentURL = window.location.href; 
+    var urlObject = new URL(currentURL); 
+  
+    var domain = urlObject.hostname;
+    var path = urlObject.pathname;
+  
+    if (path.charAt(0) === '/') {
+      path = path.substr(1);
+    }
+  
+    var gitHubEditURL = 'https://github.com/Nick-DL/nick-dl.github.io/edit/main/' + path;
+  
+    if (!window.location.href.endsWith("index.html")) {
+      window.location.href = gitHubEditURL + "index.html";
+    }
+
+    else {
+      window.location.href = gitHubEditURL;
+    }
+  }
